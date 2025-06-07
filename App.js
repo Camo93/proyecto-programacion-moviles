d7b5b25ed85c061b86424b2118d8f809c8c36545
camilo <kmylobeta@gmail.com>
2025-06-07 12:51:55 -0500

    Version estable con mejoras

--- App.js
+++ App.js
@@ -11,19 +11,24 @@ import {
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import Login from './src/view/screens/login';
-import MuscleSelection from './src/view/screens/MuscleSelection'; 
+import MuscleSelection from './src/view/screens/MuscleSelection';
+import ProfileSetup from './src/view/screens/ProfileSetup';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
   const Stack = createNativeStackNavigator();
 
-export default function App() {
+
+    
+  export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="MuscleSelection" component={MuscleSelection} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+  <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="ProfileSetup" component={ProfileSetup} />
+          <Stack.Screen name="MuscleSelection" component={MuscleSelection} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
