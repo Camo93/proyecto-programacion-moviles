f4accb37f0d5ad52c9299ff1f2e0ef1edcce82f0
camilo <kmylobeta@gmail.com>
2025-06-07 10:43:26 -0500

    Version estable

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  Alert,
+  Platform,
+} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import Login from './src/view/screens/login';
+import MuscleSelection from './src/view/screens/MuscleSelection'; 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+  const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="MuscleSelection" component={MuscleSelection} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
